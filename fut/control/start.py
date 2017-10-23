1a45b4afb518ecabbfaf642e0b94be8ec72b3360
MarcAtHTW <s0547375@htw-berlin.de>
2017-10-23 21:52:22 +0200

    - aufgeräumt

---
+++ fut/control/start.py
@@ -0,0 +1,45 @@
+import fut
+import json
+from fut.model import dbConnector as DB
+
+db = DB.Database()
+
+
+q = "CREATE TABLE Player (id INT(6) " \
+    "UNSIGNED AUTO_INCREMENT PRIMARY KEY, " \
+    "firstname VARCHAR(30) NOT NULL," \
+    "lastname VARCHAR(30) NOT NULL," \
+    "position VARCHAR(50)," \
+    "reg_date TIMESTAMP" \
+    ")"
+
+
+q = "INSERT INTO Player (firstname, lastname, position)" \
+    "VALUES ('Fu', 'Bar', 'Mittelfeld')"
+
+q = "SELECT * FROM Player"
+
+#q = "SHOW DATABASES"
+#q = "SHOW TABLES"
+
+#result = db.insert(q)
+#result = db.query(q)
+#print(result)
+fut = fut.Core('user', 'pass', 'securityanswer', debug=True)
+
+items = fut.searchAuctions(ctype='player', level='gold')
+
+dump = json.dumps(items)
+#parsed = json.loads(items)
+#print(json.dumps(parsed, indent=4, sort_keys=True))
+#print(items)
+
+#players = fut.players
+#nations = fut.nations()
+
+#leagues = fut.leagues()
+#teams = fut.teams()
+#stadiums = fut.stadiums()
+#players = fut.players()
+#playestyles = fut.playstyles()
+print('Done')
