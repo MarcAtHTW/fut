0a13ef99a717dae353b4e38e05093240512e7c9f
Basti <groundxhound@gmail.com>
2017-12-06 15:33:22 +0100

    start.py aufgeräumt

--- fut/control/start.py
+++ fut/control/start.py
@@ -41,19 +41,6 @@ fut = fut.Core(
 """ fill fut_players table """
 # loadPlayerDatabase(fut, db)
 
-"""Erfolgreiche Trades aus Watchlist in DB speichern"""
-# succesTradesFromWatchlist(fut,db)
-
-
-"""Test Query"""
-# q = "SELECT * FROM Player"
-
-"""Suche"""
-#items = fut.searchAuctions(ctype='player', page_size=48)
-# print(items)
-# items = dict()
-
-# print(len(fut.watchlist()))
 
 """Objekterzeugung Watchlist"""
 # assetIds = [20801, 158023, 167495, 176580, 190871, 188545, 155862, 156353, 167664, 182521, 183277, 193080, 1179, 153079, 173731, 177003, 184941, 192119, 192985, 9014, 41236, 164240, 176635, 178603, 182493, 183907, 184344, 188567, 189509, 194765, 200389, 211110, 238431]
@@ -69,58 +56,4 @@ watchlist = Watchlist(fut, db, assetIds, minExpireTimeInMinutes, maxExpireTimeIn
 watchlist.startBot()
 #watchlist.loadTradeIdsFromLiveWatchlist()
 
-"""Löschen der Watchlist anhand einer manuellen TradeIDListe"""
-# trade_ids = []
-# watchlist.clear(trade_ids)
-
-"""" Tests """
-#watchlist.clear()
-#watchlist.fillup(items, 10)
-#watchlist.loadItemsFromLiveWatchlist()
-
-### old loop - start ###
-
-
-
-"""" Ende Tests"""
-
-"""Befüllen der Watchliste mit den gefundenen Items der Suche"""
-# watchlist.fillup(items, 10)
-
-# print("%s players on watchlist." % len(watchlist.loadTradeIdsFromLiveWatchlist()))
-# print(fut.watchlist())
-
-"""Löschen der Watchlist zur Laufzeit"""
-# watchlist.clear()
-# print("%s players on watchlist." % len(fut.watchlist()))
-# print(fut.watchlist())
-
-# backup
-
-# for x in items:
-#    print("tradeId: " + str(x["tradeId"]) + " byNowPrice: " + str(x["buyNowPrice"]) +
-#          ' attributeListValue1: ' + str(x["attributeList"][0]["value"]))
-
-# k= list(players.keys())
-# lenghtk = len(k)
-
-# for player in players:
-#    for i in lenghtk:
-#        print(players[k[i]])
-
-# Ende backup
-
-# playerInList()
-# q = "SHOW DATABASES"
-# q = "SHOW TABLES"
-
-# result = db.insert(q)
-# result = db.query(q)
-# print(result)
-
-# parsed = json.loads(items)
-# print(json.dumps(parsed, indent=4, sort_keys=True))
-# print(items)
-
-# fut.logout()
 print('start.py Done')
