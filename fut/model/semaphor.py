e472a213feb3ea15b2950f46704a6801c3675f4e
CA283 <s0547380@htw-berlin.de>
2017-12-12 21:18:28 +0100

    Änderung an der Klasse: Semaphore Ich habe hinzugefügt wer den Lock ausgeführt hat, dann kann bei Problemen der jeweilige Thread auch die Sperre wieder aufheben und es hängen nicht beide Threads fest.

--- fut/model/semaphor.py
+++ fut/model/semaphor.py
@@ -4,21 +4,32 @@ class Semaphor:
 
     def __init__(self, fut_session):
         self.isLocked                  = False
+        self.whoLocked                 = ()
         self.session = fut_session
 
 
     def search(self, tradeId):
         while self.isLocked == True:
-            print('(Searcher): isLocked == True !')
-            sleep(1)
+            if self.isLocked == True:
+                print('(Searcher): isLocked == True ! von: {}'.format(self.whoLocked))
+                sleep(1)
+            elif self.whoLocked == 'search':
+                self.isLocked = False
+
         self.isLocked = True
+        self.whoLocked = 'search'
         self.session.sendToWatchlist(int(tradeId))
         self.isLocked = False
 
     def check(self, tradeId):
         while self.isLocked == True:
-            print('(Checker): isLocked == True !')
-            sleep(1)
+            if self.isLocked == True:
+                print('(Checker): isLocked == True ! von: {}'.format(self.whoLocked))
+                sleep(1)
+            elif self.whoLocked == 'check':
+                self.isLocked = False
+
         self.isLocked = True
+        self.whoLocked = 'check'
         self.session.watchlistDelete(tradeId)
         self.isLocked = False
