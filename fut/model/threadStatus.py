056641e1b2e5a41af9281d36478c384ff6fd2407
CA283 <s0547380@htw-berlin.de>
2017-12-13 16:18:55 +0100

    - test Commit

---
+++ fut/model/threadStatus.py
@@ -0,0 +1,21 @@
+
+
+class threadStatus:
+
+    def __init__(self):
+        self.isSearcherAlive                   = True
+        self.isCheckerAlive                    = True
+
+
+    def getSearcherStatus(self):
+        return self.isSearcherAlive
+
+    def getCheckerStatus(self):
+        return self.isCheckerAlive
+
+
+    def setSearcherStatus(self, status):
+        self.isSearcherAlive = status
+
+    def setCheckerStatus(self, status):
+        self.isCheckerAlive = status
